af4ec0c0c45cd318089656a096996ea2a7ece685
Christian Schubert <544196+SchubertCh@users.noreply.github.com>
2023-01-17 11:54:00 +0100

    Initial commit

---
+++ src/main/java/de/wadndadn/springjoptsimple/example/SpringJoptSimpleExampleApplication.java
@@ -0,0 +1,13 @@
+package de.wadndadn.springjoptsimple.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJoptSimpleExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJoptSimpleExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/wadndadn/springjoptsimple/example/SpringJoptSimpleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wadndadn.springjoptsimple.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJoptSimpleExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
